c3f6f07e0282f36b52c271716186ad2bac82246d
Rameshwarjha <jharameshwar.111@gmail.com>
2022-04-01 13:05:12 +0530

    updation_day1

---
+++ JavaScript/variable_declaration_and_use.js
@@ -0,0 +1,17 @@
+// we can declare variable with single quotes also 
+const first_name= 'Raj';   
+console.log(first_name);
+
+const last_name= "verma";
+console.log(last_name);
+
+// name with less space
+// ### how to include "variable in between string"   ** js tricks
+const full_name = `My name is ${first_name} ${last_name} `;
+console.log(full_name);
+
+// name with more space 
+// ### how to include "variable in between string"    ** js tricks
+
+const full_name1 = `My name is ${first_name}        ${last_name} `;
+console.log(full_name1);
